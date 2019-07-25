e03d81b49d56546856c062509ec80a11656c97b5
Pedro Barros <pedroedson12@hotmail.com>
2019-07-24 21:49:33 -0300

    Criação da tela sobre o app

---
+++ src/pages/TelaSobre.vue
@@ -0,0 +1,45 @@
+<template>
+  <Conteudo classeConteudo="full-height" :paddingPage="false">
+    <Titulo imagem="statics/imgs/sobre-app/001.png"/>
+
+    <div class="q-pl-sm q-pr-sm">
+      <Linha>
+        <Coluna>
+          <PVImagem imagem="statics/imgs/sobre-app/002.png"/>
+        </Coluna>
+      </Linha>
+      <Linha>
+        <Coluna>
+          <PVImagem imagem="statics/imgs/sobre-app/003.png"/>
+        </Coluna>
+      </Linha>
+
+
+      <Linha>
+        <Coluna>
+          <PVImagem imagem="statics/imgs/sobre-app/004.png"/>
+        </Coluna>
+      </Linha>
+
+
+    </div>
+  </Conteudo>
+</template>
+
+<script>
+  import Conteudo from "../components/Shared/Conteudo";
+  import Linha from "../components/Shared/Linha";
+  import Coluna from "../components/Shared/Coluna";
+  import PVImagem from "../components/PVImagem";
+  import Titulo from "../components/Shared/Titulo";
+  import Container from "../components/Shared/Container";
+  import audioMixin from "../mixins/audioMixin";
+
+  export default {
+    name: "TelaSobre",
+    components: {Container, Titulo, PVImagem, Coluna, Linha, Conteudo},
+    mixins: [audioMixin]
+
+  }
+</script>
+

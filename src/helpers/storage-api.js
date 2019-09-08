f258e08f624b186114b5f379eec728f2bf72b233
1 <1@1.com>
2019-09-08 13:16:46 -0300

    Adição do firebase storage para guardar os arquivos de audio do app

--- src/helpers/storage-api.js
+++ src/helpers/storage-api.js
@@ -1,4 +1,6 @@
 // let urlFirebase = audio => `https://firebasestorage.googleapis.com/v0/b/pedroapp-c2d3d.appspot.com/o/${audio}?alt=media`
-let urlFirebase = audio => `https://pedbarros.com.br/storage/prevtev/audios/${audio}`
+//let urlFirebase = audio => `https://pedbarros.com.br/storage/prevtev/audios/${audio}`
+
+let urlFirebase = audio => ` https://firebasestorage.googleapis.com/v0/b/pedroapp-c2d3d.appspot.com/o/${audio}?alt=media&token=67eabe57-c95a-4317-8e00-466171987c37`
 
 export { urlFirebase }
